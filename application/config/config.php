ab901c791ef97cff717105df0b8967d3d169d261
bastiannazaromi <57742252+bastiannazaromi@users.noreply.github.com>
2020-03-30 10:42:55 +0700

    edit file config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pantau-corona/';
+$config['base_url'] = 'https://pantau-covid-19.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
